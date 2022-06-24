452f259648f58230136d1f328b67dc82265f6bd3
justBearded <vanamburg.mv@gmail.com>
2022-06-23 21:53:22 -0500

    Initial Commit

---
+++ Shared/peddlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  peddlesApp.swift
+//  Shared
+//
+//  Created by Mike  Van Amburg on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct peddlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
