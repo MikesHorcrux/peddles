9e1ce9819fb12b8339a1f0fa9618f1735cb0c257
justBearded <vanamburg.mv@gmail.com>
2022-06-25 12:49:03 -0500

    the ability to get a access token from pet finder

---
+++ Shared/Models/Token/GetTokenRequest.swift
@@ -0,0 +1,18 @@
+//
+//  GetTokenRequest.swift
+//  peddles (iOS)
+//
+//  Created by Mike  Van Amburg on 6/25/22.
+//
+import Foundation
+
+struct GetTokenRequest: Request{
+    typealias ReturnType = Token
+
+    let path = "oauth2/token"
+    let method: HTTPMethod = .post
+    let client = ClientSecrets(grantType: "client_credentials", clientId: "nc0DzU2zCeHj2eQsdVsVmH1FIqOSTh2nrUzhVzXVOginMvZcvw", clientSecret: "a6mf8lAcgCrrl5CvZx4gAy0runRAn2qMBM8M2tdN")
+    var bodyData: Data? {
+        try? JSON.encoder.encode(client)
+    }
+}
---
+++ Shared/Models/Token/Token.swift
@@ -0,0 +1,12 @@
+//
+//  Token.swift
+//  peddles (iOS)
+//
+//  Created by Mike  Van Amburg on 6/25/22.
+//
+
+import Foundation
+
+struct Token: Codable {
+    var accessToken: String
+}
